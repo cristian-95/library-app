57b1340a56b46a8a5dd909102ddc31b8219d8129
cristian-95 <nem.cristianrc@gmail.com>
2023-02-10 11:37:16 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# MY LIBRARY
+
+- projeto proposto pelo curso de Javascript em  The Odin Project
+
+-   link: https://www.theodinproject.com/lessons/node-path-javascript-library
+
+## TO-DO:
+
+- [ ] Validar formulario
+- [ ] Troca de capa de livro
+- [ ] Adicionar outras caracteristicas do livro pra uso opcional
+- [ ] persistencia de dados(armazenamento local, ou nuvem)
+- [ ] revisão e otimização de app.js
---
+++ app.js
@@ -0,0 +1,130 @@
+/* constants and classes */
+const modal = document.querySelector('dialog')
+const openFormBtn = document.querySelector('#open-form-btn')
+const closeFormBtn = document.querySelector('#close-form-btn')
+const submitBtn = document.querySelector('#submit-form-btn')
+const bookData = document.querySelector('#book-data')
+const container = document.querySelector('#container')
+const clear = document.querySelector('#clear-library')
+const bookIcon = './img/book-icon.png'
+let library = []
+
+class Book {
+  constructor (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.id = Math.random().toString(16).slice(2)
+  }
+}
+
+openFormBtn.addEventListener('click', openModal)
+closeFormBtn.addEventListener('click', closeModal)
+submitBtn.addEventListener('click', readBookInfo)
+clear.addEventListener('click', clearLibrary)
+
+function openModal (e) {
+  e.preventDefault()
+  modal.showModal()
+}
+
+function closeModal (e) {
+  e.preventDefault()
+  modal.close()
+}
+
+function readBookInfo (event) {
+  event.preventDefault()
+  const formData = new FormData(bookData)
+  const data = Object.fromEntries(formData)
+  const checkbox = document.querySelector('#form-checkbox')
+  const newBook = new Book(data.title, data.author, data.pages, checkbox.checked)
+  library.push(newBook)
+  bookData.reset()
+  closeModal(event)
+  renderBooks()
+}
+
+function showBook (book) {
+  container.insertAdjacentElement('beforeend', generateCard(book))
+}
+
+function renderBooks () {
+  container.innerHTML = ''
+  library.forEach((book) => showBook(book))
+}
+
+function generateCard (book) {
+  const cardContainer = document.createElement('div')
+  const bookCard = document.createElement('div')
+  const img = document.createElement('img')
+  const h2 = document.createElement('h2')
+  const author = document.createElement('p')
+  const pages = document.createElement('p')
+  const checkboxDiv = document.createElement('div')
+  const readStatusLabel = document.createElement('label')
+  const readStatus = document.createElement('input')
+  const deleteButton = document.createElement('button')
+
+  cardContainer.classList.add('card-container')
+  bookCard.classList.add('book-card')
+  bookCard.id = book.id
+  h2.innerHTML = book.title
+  img.src = bookIcon
+  img.alt = 'book icon placeholder'
+  author.innerHTML = book.author
+  pages.innerHTML = book.pages + ' Pages'
+  checkboxDiv.classList = 'checkbox-container'
+  readStatusLabel.for = 'read'
+  readStatusLabel.innerHTML = 'Readed: '
+  readStatus.type = 'checkbox'
+  readStatus.checked = book.read
+  readStatus.name = 'read-status'
+  readStatus.id = book.id
+  readStatus.classList.add('card-checkbox')
+  deleteButton.innerHTML = 'delete'
+  deleteButton.type = 'submit'
+  deleteButton.classList.add('delete-book-btn')
+  deleteButton.id = book.id
+  
+  readStatus.addEventListener('change', toggleStatus)
+  deleteButton.addEventListener('click', deleteBook)
+
+  checkboxDiv.insertAdjacentElement('beforeend', readStatusLabel)
+  checkboxDiv.insertAdjacentElement('beforeend', readStatus)
+  bookCard.insertAdjacentElement('beforeend', h2)
+  bookCard.insertAdjacentElement('beforeend', img)
+  bookCard.insertAdjacentElement('beforeend', author)
+  bookCard.insertAdjacentElement('beforeend', pages)
+  bookCard.insertAdjacentElement('beforeend', checkboxDiv)
+  bookCard.insertAdjacentElement('beforeend', deleteButton)
+  cardContainer.insertAdjacentElement('beforeend', bookCard)
+
+  return cardContainer
+}
+
+function deleteBook () {
+  const child = document.getElementById(this.id).parentElement
+  const index = searchBookIndex(this.id)
+  container.removeChild(child)
+  library.splice(index, 1)
+}
+
+function searchBookIndex (id) {
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].id === id) {
+      return i
+    }
+  }
+}
+
+function toggleStatus () {
+  const index = searchBookIndex(this.id)
+  library[index].read = this.checked
+}
+
+function clearLibrary () {
+  container.innerHTML = ''
+  library = []
+}
